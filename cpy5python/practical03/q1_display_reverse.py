fffb1987406b28e0eef438bcd7fffa88527b0a51
natsuyuu <seiinatsuyuu@gmail.com>
2013-02-15 10:12:53 +0800

    Practical03_1

---
+++ cpy5python/practical03/q1_display_reverse.py
@@ -0,0 +1,9 @@
+#Name:q1_display_reverse.py
+#Author:Zhan Yuli
+#Description:Displaying an integer reversed
+#Created:150213
+
+def reverse_int(n):
+   print("".join(list(reversed(str(n)))))
+
+reverse_int(2345)
---
+++ cpy5python/practical03/q2_display_pattern.py
@@ -0,0 +1,9 @@
+#Name:q2_display_pattern.py
+#Author:Zhan Yuli
+#Description:Displaying patterns
+#Created:150213
+
+def display_pattern():
+
+
+display_pattern(10)
